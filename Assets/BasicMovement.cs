c4ce7e4af582318ed9be89a6d7085405f2f0db9c
Kayla Stein <kstein0119@gmail.com>
2022-04-01 13:41:39 -0400

    Jumping, Clark collision
    Clark can now jump and collides with the floor beneath him. New variables for movement speed and jump height. Does not include jump cycle animations yet.

--- Assets/BasicMovement.cs
+++ Assets/BasicMovement.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
+    public float moveSpeed = 3f;
     public Animator animator;
+    public bool isGrounded = false;
+    public float jumpHeight = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +18,25 @@ public class BasicMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Jump(); 
+
         animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
 
-        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
-        transform.position = transform.position + horizontal * Time.deltaTime * 3;
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position = transform.position + horizontal * Time.deltaTime * moveSpeed;
 
 
     }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true) {
+
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+
+        }
+
+
+    }
+
 }
