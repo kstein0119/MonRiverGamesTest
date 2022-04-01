9a54b2c9ed62f0f5bc6a8c79228586775dc22999
Kayla Stein <kstein0119@gmail.com>
2022-04-01 16:35:02 -0400

    Health system, orientation
    Started health system: unable to get objects to cause damage yet. Fixed orientation of Clark so he's always upright.

---
+++ Assets/HealthController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+
+    public int playerHealth;
+    [SerializeField] private Image[] hearts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateHealth();
+    }
+
+    public void UpdateHealth()
+    {
+
+        /* 
+        if (playerHealth <= 0)
+        {
+            Restart the game/respawn
+        }
+        */ 
+
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < playerHealth)
+            {
+                hearts[i].color = Color.red;
+            } else
+            {
+                hearts[i].color = Color.black;
+            }
+        }
+    }
+
+
+}
